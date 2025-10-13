a1cf1ccb51b702288bae5fd596dcca91e78e93fe
Billy Xu <billy061005@outlook.com>
2025-10-13 01:09:20 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://techlef.xyz/",
+	site: "https://techleaf.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
